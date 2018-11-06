d9502c0b96d1422f73291f456c2a15e2e482565f
saad <saadanjum69@gmail.com>
2018-11-06 18:54:40 +0500

    New Update

--- src/pages/coin-comparison/coin-comparison.ts
+++ src/pages/coin-comparison/coin-comparison.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { AccountPage } from '../account/account';
@@ -16,64 +16,88 @@ import { ApiServiceProvider } from '../../providers/api-service/api-service';
   selector: 'page-coin-comparison',
   templateUrl: 'coin-comparison.html',
 })
-export class CoinComparisonPage {
+export class CoinComparisonPage implements OnInit {
 
   coinComparison;
   coinComparsionName;
-  input;
-  coinComparisonAddCoin;
+  input="90";
+  coinComparisonAddCoin: Array<any> = [];
+  name;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private api:ApiServiceProvider) {
   }
 
   ionViewDidEnter(){
-    this.getCoinComparison();
-    this.getCoinComparisonName('eth');
+   
+   
   }
 
   ngOnInit(){
-this.getCoinComparisonAddCoin();
+    this.getCoinComparison(); 
   }
   getCoinComparison(){
-    this.api.getCoinComparison().subscribe( (data) => {
-     this.coinComparison = data
-     console.log("CoinComparison",  this.coinComparison);
-   })
+    this.api.getCoinComparisonAddCoin('BTC', this.input).subscribe( (data) => {
+      this.coinComparisonAddCoin.push({name: 'BTC', ...data[0]});
+      console.log(this.coinComparisonAddCoin)
+    })
  }
  getCoinComparisonName(name){
   this.api.getCoinComparisonName(name).subscribe( (data) => {
-    this.coinComparsionName = data
-    console.log("CoinComparisonName",  this.coinComparsionName);
+    this.coinComparsionName = data;
+
   })
+ }
+
+ getCoin(name){
+   this.getCoinComparisonAddCoin(name);
  }
  onSearch(event){
    
    if(event==""){
-     this.coinComparsionName=null;  
+     this.coinComparsionName="";  
    }
    else{
-    this.getCoinComparisonName(event);
+ this.getCoinComparisonName(event);
+
    }
 //  this.name+=event.target.value;
- console.log(event);
 
  }
 
- getCoinComparisonAddCoin(){
-   this.api.getCoinComparisonAddCoin(this.coinComparsionName, this.input).subscribe( (data) => {
-    this.coinComparisonAddCoin = data
-    console.log("coinComparisonAddCoins",  this.coinComparisonAddCoin);
+ getCoinComparisonAddCoin(name){
+   this.name = name;
+   this.api.getCoinComparisonAddCoin(name, this.input).subscribe( (data) => {
+    this.coinComparisonAddCoin.push({name: this.name, ...data[0]});
+    if(this.coinComparisonAddCoin){
+      this.coinComparsionName=false;
+    }
   })
  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CoinComparisonPage');
   }
   ClickToHomePage(){
     this.navCtrl.setRoot(HomePage);
   }
   clickToAccountPage(){
     this.navCtrl.setRoot(AccountPage);
+  }
+
+  onChange(event){
+    this.input = event;
+
+    for(let i =0; i< this.coinComparisonAddCoin.length; i++){
+
+      this.setCoins(this.coinComparisonAddCoin[i].name,i);
+    }
+
+
+  }
+
+  setCoins(nae,i){
+    this.api.getCoinComparisonAddCoin(nae, this.input).subscribe( (data) => {
+      this.coinComparisonAddCoin[i] = {name: nae, ...data[0]};
+    })
   }
 
 }
